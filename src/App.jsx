c41a6a0a20b0811545aa62e507f6ae3ff578902e
premm-learnings <164186851+premm-learnings@users.noreply.github.com>
2026-02-23 15:16:09 +0530

    add root route redirect

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
 
 
         {/* Public Routes */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
